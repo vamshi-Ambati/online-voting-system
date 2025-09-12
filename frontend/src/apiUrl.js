f2d4a5e014dc9b921285b91b5f578488a8b0cbcb
vamshi-Ambati <vamshiambati27@gmail.com>
2025-09-12 15:23:00 +0530

    changes

--- frontend/src/apiUrl.js
+++ frontend/src/apiUrl.js
@@ -1,4 +1,4 @@
 // const apiUrl = "http://localhost:3000";
-const apiUrl = "https://online-voting-system-f08m.onrender.com/";
+const apiUrl = "https://online-voting-system-f08m.onrender.com";
 
 export default apiUrl;
